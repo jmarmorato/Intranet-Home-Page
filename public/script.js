6bb0d036f03513791bdbb082da3bce34f7c01f5a
root <justinmarmorato@gmail.com>
2022-04-18 08:10:37 +0000

    Cleaned up code

--- public/script.js
+++ public/script.js
@@ -1,72 +1,46 @@
 /*
-*  Form helper functions
+*  Form submission functions
 */
 
-function searchAutoComplete() {
-  var url = "/GoogleAutoComplete";
-  var query = document.getElementById("appendedInputButton").value;
-
-  $.post(url, {q:query}, function(response){
-
-    console.log(response);
-
-    res_obj = JSON.parse(response);
-
-    console.log(res_obj[1]);
-  });
-
-}
-
 function googleSearch() {
-  document.getElementById("appendedInputButton").setAttribute("name","q");
-  document.getElementById("searchForm").setAttribute("action","https://www.google.com/search");
-  document.getElementById("searchForm").submit();
+  $("#appendedInputButton").attr("name","q");
+  $("#searchForm").attr("action","https://www.google.com/search");
+  $("#searchForm")[0].submit();
 }
 
 function redditSearch() {
-  document.getElementById("appendedInputButton").setAttribute("name","q");
-  document.getElementById("searchForm").setAttribute("action","https://www.reddit.com/search/");
-  document.getElementById("searchForm").submit();
+  $("#appendedInputButton").attr("name","q");
+  $("#searchForm").attr("action","https://www.reddit.com/search/");
+  $("#searchForm")[0].submit();
 }
 
 function youtubeSearch() {
-  document.getElementById("appendedInputButton").setAttribute("name","search_query");
-  document.getElementById("searchForm").setAttribute("action","https://www.youtube.com/results");
-  document.getElementById("searchForm").submit();
+  $("#appendedInputButton").attr("name","search_query");
+  $("#searchForm").attr("action","https://www.youtube.com/results");
+  $("#searchForm")[0].submit();
 }
 
 function submitNewRecord() {
-  document.getElementById("newRecordForm").submit();
+  $("#newRecordForm")[0].submit();
 }
 
 /*
 *   Event Bindings
 */
 
-var searchInput = document.getElementById("appendedInputButton");
-searchInput.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-
-    console.log("Search");
-  }
-});
-
 //Capture enter key and run Google search
 $("#appendedInputButton").keyup(function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     googleSearch();
-  } else {
-    //Run the autocomplete function for each character typed in the search
-    //searchAutoComplete();
   }
 });
 
+//Attach autocomplete
 $('#appendedInputButton').autocomplete({
-    serviceUrl: '/GoogleAutoComplete',
-    onSelect: function (suggestion) {
-        $('#appendedInputButton').value = suggestion.value;
-        googleSearch()
-    }
+  serviceUrl: '/GoogleAutoComplete',
+  onSelect: function (suggestion) {
+    $('#appendedInputButton').value = suggestion.value;
+    googleSearch()
+  }
 });
