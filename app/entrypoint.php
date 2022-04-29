46fcb91e475e198cc61cf7a3229be31befc1e77b
jmarmorato <justinmarmorato@gmail.com>
2022-04-29 02:10:32 -0400

    Fix environment variables in entrypoint

--- app/entrypoint.php
+++ app/entrypoint.php
@@ -2,8 +2,8 @@
 
 $installed = false;
 
-require_once "init.php"
+require_once "init.php";
 
-$result = Installer::build_database($_SERVER["host"], $_SERVER["user"], $_SERVER["pass"], $_SERVER["schema"], APPPATH);
+$result = Installer::build_database($_SERVER["DB_HOST"], $_SERVER["DB_USER"], $_SERVER["DB_PASS"], $_SERVER["DB_SCHEMA"], APPPATH);
 
 ?>
